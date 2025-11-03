2616a0b4dbe1845b08a1a3b67a5c53180814f39b
faizaizattt <faizaizat1001@gmail.com>
2025-11-03 17:52:50 +0800

    create files package

---
+++ android/app/src/main/kotlin/com/barbarianking/edupulse_qr/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.barbarianking.edupulse_qr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/utils/constants.dart
@@ -0,0 +1,4 @@
+const String appTitle = "EduPulse QR";
+const String menuKedatangan = "Kedatangan";
+const String menuRMT = "RMT";
+const String menuSahsiah = "Sahsiah Diri";
